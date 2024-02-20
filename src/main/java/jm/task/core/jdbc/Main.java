5fe6d3e22c0a8f839ab828bcb2bda552ad8a57b7
MegaVirus93 <MegaVirus93@gmail.com>
2024-02-20 12:25:17 +0300

    1.1.5 (1.1.3). Работа с базой данных c помощью Hibernate. Исправлено: 1. Читающие методы 2. Добавлен метод rollback() 3. Вывод с помошью forEach()

--- src/main/java/jm/task/core/jdbc/Main.java
+++ src/main/java/jm/task/core/jdbc/Main.java
@@ -12,12 +12,11 @@ public class Main {
         UserService userService = new UserServiceImpl();
         userService.createUsersTable();
 
-        userService.saveUser("Витя", "Иванов", (byte) 19);
+        userService.saveUser("Виктор", "Иванов", (byte) 19);
         userService.saveUser("Олег", "Сергеев", (byte) 20);
         userService.saveUser("Сергей", "Петров", (byte) 21);
         userService.saveUser("Максим", "Сидоров", (byte) 23);
-        List<User> users = userService.getAllUsers();
-        System.out.println(users);
+        userService.getAllUsers().forEach(System.out::println);
 
         userService.cleanUsersTable();
         userService.dropUsersTable();
